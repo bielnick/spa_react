a50f049e2bfffc31fa016772f1d8eb9a2911e029
Gabriel <gabriel.lima@aoop.com.br>
2019-12-30 13:53:32 -0300

    Componentes MenuSuperior e Resumo

---
+++ src/components/MenuSuperior/MenuSuperior.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MenuSuperior extends Component{
+    render(){
+        return ( 
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+           <div className="navbar-expand">Gabriel</div>
+        </nav>
+        )
+    }
+}
+
+export default MenuSuperior;
